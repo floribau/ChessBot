5f99a1c4bcb4e495bd5384b2eb03310bdea22395
Florian Bauer <fpb255@gmx.de>
2024-07-01 14:42:00 +0200

    #1 fix style sheet

--- src/main/java/GUI/GUIConfig.java
+++ src/main/java/GUI/GUIConfig.java
@@ -22,4 +22,11 @@ public class GUIConfig {
   public static Image PAWN_WHITE = new Image(ClassLoader.getSystemResource("images/pawn-white.png").toString());
   public static Image PAWN_BLACK = new Image(ClassLoader.getSystemResource("images/pawn-black.png").toString());
 
+  public static String colorToHexString(Color color){
+    int r = (int) (color.getRed() * 255);
+    int g = (int) (color.getGreen() * 255);
+    int b = (int) (color.getBlue() * 255);
+
+    return String.format("#%02X%02X%02X", r, g, b);
+  }
 }
