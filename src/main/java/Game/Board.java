40105e80c82f5659818e6407731731f391887509
Florian Bauer <fpb255@gmx.de>
2024-07-10 13:55:42 +0200

    #3 move piece list and init to Board.java

--- src/main/java/Game/Board.java
+++ src/main/java/Game/Board.java
@@ -1,9 +1,12 @@
 package Game;
 
 import Util.Position;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
   String[][] board;
+  private List<Piece> pieces;
 
   public Board(){
     this.board = new String[8][8];
@@ -17,6 +20,114 @@ public class Board {
         this.board[i][j] = board[i][j];
       }
     }
+  }
+  
+  public void initBoard(){
+    pieces = new ArrayList<>();
+
+    Piece king_white = new Piece(PieceType.KING_WHITE, PlayerColor.WHITE);
+    Piece queen_white = new Piece(PieceType.QUEEN_WHITE, PlayerColor.WHITE);
+    Piece rook_white_0 = new Piece(PieceType.ROOK_WHITE, PlayerColor.WHITE);
+    Piece rook_white_1 = new Piece(PieceType.ROOK_WHITE, PlayerColor.WHITE);
+    Piece bishop_white_0 = new Piece(PieceType.BISHOP_WHITE, PlayerColor.WHITE);
+    Piece bishop_white_1 = new Piece(PieceType.BISHOP_WHITE, PlayerColor.WHITE);
+    Piece knight_white_0 = new Piece(PieceType.KNIGHT_WHITE, PlayerColor.WHITE);
+    Piece knight_white_1 = new Piece(PieceType.KNIGHT_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_0 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_1 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_2 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_3 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_4 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_5 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_6 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+    Piece pawn_white_7 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
+
+    pieces.add(king_white);
+    pieces.add(queen_white);
+    pieces.add(rook_white_0);
+    pieces.add(rook_white_1);
+    pieces.add(bishop_white_0);
+    pieces.add(bishop_white_1);
+    pieces.add(knight_white_0);
+    pieces.add(knight_white_1);
+    pieces.add(pawn_white_0);
+    pieces.add(pawn_white_1);
+    pieces.add(pawn_white_2);
+    pieces.add(pawn_white_3);
+    pieces.add(pawn_white_4);
+    pieces.add(pawn_white_5);
+    pieces.add(pawn_white_6);
+    pieces.add(pawn_white_7);
+
+    Piece king_black = new Piece(PieceType.KING_BLACK, PlayerColor.BLACK);
+    Piece queen_black = new Piece(PieceType.QUEEN_BLACK, PlayerColor.BLACK);
+    Piece rook_black_0 = new Piece(PieceType.ROOK_BLACK, PlayerColor.BLACK);
+    Piece rook_black_1 = new Piece(PieceType.ROOK_BLACK, PlayerColor.BLACK);
+    Piece bishop_black_0 = new Piece(PieceType.BISHOP_BLACK, PlayerColor.BLACK);
+    Piece bishop_black_1 = new Piece(PieceType.BISHOP_BLACK, PlayerColor.BLACK);
+    Piece knight_black_0 = new Piece(PieceType.KNIGHT_BLACK, PlayerColor.BLACK);
+    Piece knight_black_1 = new Piece(PieceType.KNIGHT_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_0 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_1 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_2 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_3 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_4 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_5 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_6 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+    Piece pawn_black_7 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
+
+    pieces.add(king_black);
+    pieces.add(queen_black);
+    pieces.add(rook_black_0);
+    pieces.add(rook_black_1);
+    pieces.add(bishop_black_0);
+    pieces.add(bishop_black_1);
+    pieces.add(knight_black_0);
+    pieces.add(knight_black_1);
+    pieces.add(pawn_black_0);
+    pieces.add(pawn_black_1);
+    pieces.add(pawn_black_2);
+    pieces.add(pawn_black_3);
+    pieces.add(pawn_black_4);
+    pieces.add(pawn_black_5);
+    pieces.add(pawn_black_6);
+    pieces.add(pawn_black_7);
+
+    this.setPieceAt(new Position(0,0), rook_black_0.getId());
+    this.setPieceAt(new Position(0, 1), knight_black_0.getId());
+    this.setPieceAt(new Position(0, 2), bishop_black_0.getId());
+    this.setPieceAt(new Position(0, 3), queen_black.getId());
+    this.setPieceAt(new Position(0, 4), king_black.getId());
+    this.setPieceAt(new Position(0, 5), bishop_black_1.getId());
+    this.setPieceAt(new Position(0, 6), knight_black_1.getId());
+    this.setPieceAt(new Position(0, 7), rook_black_1.getId());
+
+    this.setPieceAt(new Position(1, 0), pawn_black_0.getId());
+    this.setPieceAt(new Position(1, 1), pawn_black_1.getId());
+    this.setPieceAt(new Position(1, 2), pawn_black_2.getId());
+    this.setPieceAt(new Position(1, 3), pawn_black_3.getId());
+    this.setPieceAt(new Position(1, 4), pawn_black_4.getId());
+    this.setPieceAt(new Position(1, 5), pawn_black_5.getId());
+    this.setPieceAt(new Position(1, 6), pawn_black_6.getId());
+    this.setPieceAt(new Position(1, 7), pawn_black_7.getId());
+
+    this.setPieceAt(new Position(7, 0), rook_white_0.getId());
+    this.setPieceAt(new Position(7, 1), knight_white_0.getId());
+    this.setPieceAt(new Position(7, 2), bishop_white_0.getId());
+    this.setPieceAt(new Position(7, 3), queen_white.getId());
+    this.setPieceAt(new Position(7, 4), king_white.getId());
+    this.setPieceAt(new Position(7, 5), bishop_white_1.getId());
+    this.setPieceAt(new Position(7, 6), knight_white_1.getId());
+    this.setPieceAt(new Position(7, 7), rook_white_1.getId());
+
+    this.setPieceAt(new Position(6, 0), pawn_white_0.getId());
+    this.setPieceAt(new Position(6, 1), pawn_white_1.getId());
+    this.setPieceAt(new Position(6, 2), pawn_white_2.getId());
+    this.setPieceAt(new Position(6, 3), pawn_white_3.getId());
+    this.setPieceAt(new Position(6, 4), pawn_white_4.getId());
+    this.setPieceAt(new Position(6, 5), pawn_white_5.getId());
+    this.setPieceAt(new Position(6, 6), pawn_white_6.getId());
+    this.setPieceAt(new Position(6, 7), pawn_white_7.getId());
   }
 
   public void move(Position oldPos, Position newPos){
@@ -59,4 +170,7 @@ public class Board {
     return res;
   }
 
+  public List<Piece> getPieces() {
+    return pieces;
+  }
 }
--- src/main/java/Game/GameEngine.java
+++ src/main/java/Game/GameEngine.java
@@ -1,5 +1,6 @@
 package Game;
 
+import AI.AIPlayer;
 import GUI.ChessBoardController;
 import Util.Position;
 import java.util.List;
@@ -7,141 +8,45 @@ import java.util.ArrayList;
 
 public class GameEngine {
   private static Board currentBoard;
-  private static List<Piece> pieces;
-  private static PlayerColor humanPlayerColor;
-  private static PlayerColor currentPlayerColor;
+
+  private static Player player1;
+  private static Player player2;
+  private static Player currentPlayer;
   private static ChessBoardController controller;
 
-  public synchronized static void initBoard(){
-    pieces = new ArrayList<>();
+  public synchronized static void startGame(ChessBoardController controller){
+    // TODO implement logic to select game mode
+    GameEngine.player1 = new Player(PlayerColor.WHITE, true);
+    GameEngine.currentPlayer = player1;
+    GameEngine.player2 = new AIPlayer(PlayerColor.BLACK);
+
     currentBoard = new Board();
+    currentBoard.initBoard();
 
-    Piece king_white = new Piece(PieceType.KING_WHITE, PlayerColor.WHITE);
-    Piece queen_white = new Piece(PieceType.QUEEN_WHITE, PlayerColor.WHITE);
-    Piece rook_white_0 = new Piece(PieceType.ROOK_WHITE, PlayerColor.WHITE);
-    Piece rook_white_1 = new Piece(PieceType.ROOK_WHITE, PlayerColor.WHITE);
-    Piece bishop_white_0 = new Piece(PieceType.BISHOP_WHITE, PlayerColor.WHITE);
-    Piece bishop_white_1 = new Piece(PieceType.BISHOP_WHITE, PlayerColor.WHITE);
-    Piece knight_white_0 = new Piece(PieceType.KNIGHT_WHITE, PlayerColor.WHITE);
-    Piece knight_white_1 = new Piece(PieceType.KNIGHT_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_0 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_1 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_2 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_3 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_4 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_5 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_6 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-    Piece pawn_white_7 = new Piece(PieceType.PAWN_WHITE, PlayerColor.WHITE);
-
-    pieces.add(king_white);
-    pieces.add(queen_white);
-    pieces.add(rook_white_0);
-    pieces.add(rook_white_1);
-    pieces.add(bishop_white_0);
-    pieces.add(bishop_white_1);
-    pieces.add(knight_white_0);
-    pieces.add(knight_white_1);
-    pieces.add(pawn_white_0);
-    pieces.add(pawn_white_1);
-    pieces.add(pawn_white_2);
-    pieces.add(pawn_white_3);
-    pieces.add(pawn_white_4);
-    pieces.add(pawn_white_5);
-    pieces.add(pawn_white_6);
-    pieces.add(pawn_white_7);
-
-    Piece king_black = new Piece(PieceType.KING_BLACK, PlayerColor.BLACK);
-    Piece queen_black = new Piece(PieceType.QUEEN_BLACK, PlayerColor.BLACK);
-    Piece rook_black_0 = new Piece(PieceType.ROOK_BLACK, PlayerColor.BLACK);
-    Piece rook_black_1 = new Piece(PieceType.ROOK_BLACK, PlayerColor.BLACK);
-    Piece bishop_black_0 = new Piece(PieceType.BISHOP_BLACK, PlayerColor.BLACK);
-    Piece bishop_black_1 = new Piece(PieceType.BISHOP_BLACK, PlayerColor.BLACK);
-    Piece knight_black_0 = new Piece(PieceType.KNIGHT_BLACK, PlayerColor.BLACK);
-    Piece knight_black_1 = new Piece(PieceType.KNIGHT_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_0 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_1 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_2 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_3 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_4 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_5 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_6 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-    Piece pawn_black_7 = new Piece(PieceType.PAWN_BLACK, PlayerColor.BLACK);
-
-    pieces.add(king_black);
-    pieces.add(queen_black);
-    pieces.add(rook_black_0);
-    pieces.add(rook_black_1);
-    pieces.add(bishop_black_0);
-    pieces.add(bishop_black_1);
-    pieces.add(knight_black_0);
-    pieces.add(knight_black_1);
-    pieces.add(pawn_black_0);
-    pieces.add(pawn_black_1);
-    pieces.add(pawn_black_2);
-    pieces.add(pawn_black_3);
-    pieces.add(pawn_black_4);
-    pieces.add(pawn_black_5);
-    pieces.add(pawn_black_6);
-    pieces.add(pawn_black_7);
-
-    currentBoard.setPieceAt(new Position(0,0), rook_black_0.getId());
-    currentBoard.setPieceAt(new Position(0, 1), knight_black_0.getId());
-    currentBoard.setPieceAt(new Position(0, 2), bishop_black_0.getId());
-    currentBoard.setPieceAt(new Position(0, 3), queen_black.getId());
-    currentBoard.setPieceAt(new Position(0, 4), king_black.getId());
-    currentBoard.setPieceAt(new Position(0, 5), bishop_black_1.getId());
-    currentBoard.setPieceAt(new Position(0, 6), knight_black_1.getId());
-    currentBoard.setPieceAt(new Position(0, 7), rook_black_1.getId());
-
-    currentBoard.setPieceAt(new Position(1, 0), pawn_black_0.getId());
-    currentBoard.setPieceAt(new Position(1, 1), pawn_black_1.getId());
-    currentBoard.setPieceAt(new Position(1, 2), pawn_black_2.getId());
-    currentBoard.setPieceAt(new Position(1, 3), pawn_black_3.getId());
-    currentBoard.setPieceAt(new Position(1, 4), pawn_black_4.getId());
-    currentBoard.setPieceAt(new Position(1, 5), pawn_black_5.getId());
-    currentBoard.setPieceAt(new Position(1, 6), pawn_black_6.getId());
-    currentBoard.setPieceAt(new Position(1, 7), pawn_black_7.getId());
-
-    currentBoard.setPieceAt(new Position(7, 0), rook_white_0.getId());
-    currentBoard.setPieceAt(new Position(7, 1), knight_white_0.getId());
-    currentBoard.setPieceAt(new Position(7, 2), bishop_white_0.getId());
-    currentBoard.setPieceAt(new Position(7, 3), queen_white.getId());
-    currentBoard.setPieceAt(new Position(7, 4), king_white.getId());
-    currentBoard.setPieceAt(new Position(7, 5), bishop_white_1.getId());
-    currentBoard.setPieceAt(new Position(7, 6), knight_white_1.getId());
-    currentBoard.setPieceAt(new Position(7, 7), rook_white_1.getId());
-
-    currentBoard.setPieceAt(new Position(6, 0), pawn_white_0.getId());
-    currentBoard.setPieceAt(new Position(6, 1), pawn_white_1.getId());
-    currentBoard.setPieceAt(new Position(6, 2), pawn_white_2.getId());
-    currentBoard.setPieceAt(new Position(6, 3), pawn_white_3.getId());
-    currentBoard.setPieceAt(new Position(6, 4), pawn_white_4.getId());
-    currentBoard.setPieceAt(new Position(6, 5), pawn_white_5.getId());
-    currentBoard.setPieceAt(new Position(6, 6), pawn_white_6.getId());
-    currentBoard.setPieceAt(new Position(6, 7), pawn_white_7.getId());
-
-    controller.disableAllButtons();
-    if(currentPlayerColor == humanPlayerColor) {
-      activateHumanButtons();
-    }
-    controller.repaint(currentBoard.getBoard());
-  }
-
-  public static void startGame(ChessBoardController controller){
-   humanPlayerColor = Math.random() >= 0.5 ? PlayerColor.WHITE : PlayerColor.BLACK;
-   startGame(humanPlayerColor, controller);
-  }
-
-  public synchronized static void startGame(PlayerColor playerColor, ChessBoardController controller){
-    GameEngine.humanPlayerColor = playerColor;
-    GameEngine.currentPlayerColor = PlayerColor.WHITE;
     GameEngine.controller = controller;
-    System.out.println("Controller at line 108: " + controller);
-    initBoard();
+    controller.disableAllButtons();
+    controller.repaint(currentBoard.getBoard());
+
+    playGame();
+  }
+
+  public synchronized static void playGame(){
+    while(!isGameOver()) {
+      currentPlayer.makeMove();
+      switchCurrentPlayer();
+    }
+  }
+
+  public synchronized  static void switchCurrentPlayer(){
+    if (currentPlayer == player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
   }
 
   public static Piece getPieceById(String id) {
-    for(Piece piece : pieces){
+    for(Piece piece : currentBoard.getPieces()){
       if(piece.getId() == id){
         return piece;
       }
@@ -149,8 +54,12 @@ public class GameEngine {
     return null;
   }
 
-  public static PlayerColor getHumanPlayerColor(){
-    return humanPlayerColor;
+  public static Player getPlayer1(){
+    return player1;
+  }
+
+  public static Player getPlayer2() {
+    return player2;
   }
 
   public static Board getCurrentBoard(){
@@ -175,7 +84,7 @@ public class GameEngine {
 
   public static List<Piece> calcMovablePieces(PlayerColor player, Board board) {
     List<Piece> movablePieces = new ArrayList<>();
-    for(Piece piece : pieces){
+    for(Piece piece : currentBoard.getPieces()){
       if(piece.getColor() == player) {
         // TODO check if piece is movable
         movablePieces.add(piece);
@@ -188,10 +97,15 @@ public class GameEngine {
     return true;
   }
 
-  public static void activateHumanButtons(){
-    for(Piece piece : calcMovablePieces(PlayerColor.WHITE, currentBoard)){
+  public static void activateColorButtons(PlayerColor color){
+    for(Piece piece : calcMovablePieces(color, currentBoard)){
       controller.activateButton(currentBoard.getPositionOfPiece(piece.getId()));
     }
+  }
+
+  //TODO implement
+  public static boolean isGameOver(){
+    return true;
   }
 
 }
