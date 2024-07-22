ba9dc4045b7013e7ff83f6c34cf2a317ec3b006c
Florian Bauer <fpb255@gmx.de>
2024-07-22 14:49:57 +0200

    #2 adjust AIConfig weights

--- src/main/java/AI/AIConfig.java
+++ src/main/java/AI/AIConfig.java
@@ -5,7 +5,7 @@ public class AIConfig {
   public static float mobilityWeight = 0.01f;
   public static float pawnStructureWeight = 0.25f;
   public static float centerControlWeight = 0.25f;
-  public static float developmentWeight = 0.05f;
+  public static float developmentWeight = 0.025f;
   public static float kingSafetyWeight = 0.05f;
 
 }
